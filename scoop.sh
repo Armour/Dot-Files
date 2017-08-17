46b2e26bf853c203acf7c89fe9a9f8ff855a44c4
Chong Guo <armourcy@gmail.com>
2017-08-16 23:57:58 -0700

    updated scoop.sh

--- scoop.sh
+++ scoop.sh
@@ -1,5 +1,4 @@
+set-executionpolicy unrestricted -s cu
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-scoop install 7zip coreutils curl git grep openssh sed wget vim grep 
-scoop install concfg
-scoop install pshazz
+scoop install 7zip coreutils curl git grep openssh sed wget vim grep yarn concfg pshazz
 concfg import solarized small
