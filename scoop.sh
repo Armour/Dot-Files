a8822a16070af45f7940b9dedc4888c5b981f77b
Armour <497052684@qq.com>
2017-08-16 19:44:43 -0700

    added scoop.sh for windows

---
+++ scoop.sh
@@ -0,0 +1,5 @@
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install 7zip coreutils curl git grep openssh sed wget vim grep 
+scoop install concfg
+scoop install pshazz
+concfg import solarized small
