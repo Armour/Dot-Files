e12677c60ea71155ee808467e89d3e5c92baa813
Armour <497052684@qq.com>
2017-08-18 22:53:56 -0700

    updated brew and scoop

--- brew.sh
+++ brew.sh
@@ -1,5 +1,5 @@
 brew update
 brew upgrade
 brew install autojump docker-clean heroku libpng mono nginx node postgresql redis yarn wget python pyenv
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package quicklookase qlvideo quicklookapk quicklook-pat qladdict macdown betterzip neteasemusic teamviewer skype google-chrome atom slack lingon-x shadowsocksx appzapper iterm2 scroll-reverser dash unity postico rowanj-gitx minecraft dropbox alfred docker visual-studio-code spectacle spotify
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package quicklookase qlvideo quicklookapk quicklook-pat qladdict macdown betterzip neteasemusic teamviewer skype google-chrome atom slack lingon-x shadowsocksx appzapper iterm2 scroll-reverser dash unity postico rowanj-gitx minecraft dropbox alfred docker visual-studio-code spectacle spotify appcode pycharm clion
 brew cleanup
--- scoop.sh
+++ scoop.sh
@@ -1,5 +1,3 @@
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-scoop install 7zip coreutils curl git grep openssh sed wget vim grep 
-scoop install concfg
-scoop install pshazz
+scoop install 7zip coreutils curl git grep openssh sed wget vim grep python concfg pshazz
 concfg import solarized small
