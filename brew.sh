8ac9459f964af4613aba0491b8e46a06c1eb3fb2
Chong Guo <armourg@sfu.ca>
2017-10-24 22:07:02 -0700

    updated brew.sh

--- brew.sh
+++ brew.sh
@@ -2,5 +2,5 @@ brew tap buo/cask-upgrade homebrew/python homebrew/science homebrew/services
 brew update
 brew upgrade
 brew install autojump docker-clean heroku libpng mono nginx node postgresql redis yarn wget python python3 sqlmap taglib tree
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize qlvideo quicklookapk qladdict macdown betterzipql neteasemusic teamviewer skype google-chrome slack lingon-x shadowsocksx appzapper iterm2 scroll-reverser dash unity postico rowanj-gitx dropbox alfred docker visual-studio-code spectacle spotify appcode pycharm clion webstorm background-music alarm-clock museeks qq nylas-mail kitematic discord insomnia db-browser-for-sqlite
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize qlvideo quicklookapk qladdict macdown betterzipql neteasemusic teamviewer skype google-chrome slack lingon-x shadowsocksx-ng appzapper iterm2 scroll-reverser dash unity postico rowanj-gitx dropbox alfred docker visual-studio-code spectacle spotify appcode pycharm clion webstorm background-music alarm-clock museeks qq nylas-mail kitematic discord insomnia db-browser-for-sqlite
 brew cleanup
