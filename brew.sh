b1fd5aee77cc6902e73dd0d03589496cf597c258
Chong Guo <armourg@sfu.ca>
2018-01-10 16:54:08 -0800

    updated config

--- brew.sh
+++ brew.sh
@@ -3,13 +3,13 @@ brew update
 brew upgrade
 
 # install brew stuff
-brew install autojump docker-clean heroku libpng mono nginx node postgresql redis yarn wget python python3 sqlmap taglib tree ccat archey colordiff stormssh
+brew install autojump docker-clean heroku libpng mono nginx node postgresql redis yarn wget python python3 sqlmap taglib tree ccat archey colordiff stormssh htop
 
 # install brew cask stuff
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize qlvideo quicklookapk qladdict macdown betterzipql neteasemusic teamviewer google-chrome slack lingon-x shadowsocksx-ng appzapper iterm2 scroll-reverser dash unity postico rowanj-gitx dropbox alfred docker visual-studio-code spectacle alarm-clock lyricsx qq nylas-mail kitematic db-browser-for-sqlite burp-suite jupyter-notebook-ql oversight vlc vox font-source-code-pro-for-powerline font-fira-code google-cloud-sdk cheatsheet usage
 
 # install brew cask stuff (optional)
-brew cask install wireshark texmaker mactex skype spotify appcode clion pycharm webstorm clip-studio-paint telegram insomnia kaleidoscope
+brew cask install wireshark texmaker mactex skype spotify appcode clion pycharm webstorm clip-studio-paint telegram insomnia kaleidoscope now dashlane
 
 brew cleanup
 brew cask cleanup
