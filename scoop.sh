357401d441ce09e6a9c78baa973df3b462cc9070
Chong Guo <armourcy@gmail.com>
2018-05-22 14:03:36 -0700

    Update scoop.sh

--- scoop.sh
+++ scoop.sh
@@ -1,6 +1,10 @@
 set-executionpolicy unrestricted -s cu
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
 scoop install sudo 7zip coreutils curl git grep openssh sed wget vim grep concfg pshazz nodejs yarn less go
-scoop bucket add extras
-scoop install vscode anaconda3 hyper
 concfg import material
+
+scoop bucket add extras 
+scoop bucket add nerd-fonts
+scoop install vscode anaconda3 hyper kitematic now-cli p4merge postman shadowsocks slack telegram gcloud
+sudo scoop install FiraCode-NF SourceCodePro-NF
