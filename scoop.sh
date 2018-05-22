eed285e4bddfcc58e8e492324c012d026c1a5f77
Chong Guo <armourcy@gmail.com>
2018-05-21 17:08:10 -0700

    Update scoop.sh

--- scoop.sh
+++ scoop.sh
@@ -1,4 +1,6 @@
 set-executionpolicy unrestricted -s cu
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-scoop install 7zip coreutils curl git grep openssh sed wget vim grep python concfg pshazz yarn
-concfg import solarized small
+scoop install sudo 7zip coreutils curl git grep openssh sed wget vim grep concfg pshazz nodejs yarn less go
+scoop bucket add extras
+scoop install vscode anaconda3 hyper
+concfg import material
